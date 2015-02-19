f6204af15fde5cc2fcb5ef8a506959bddd7bac57
SharerMax <mdcw1103@gmail.com>
2015-02-20 01:12:26 +0800

    RecyclerView and CardView

---
+++ README.md
@@ -0,0 +1,3 @@
+#M-News
+
+A News Client
--- app/src/main/java/net/sharermax/m_news/MainActivity.java
+++ app/src/main/java/net/sharermax/m_news/MainActivity.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,6 +25,26 @@ public class MainActivity extends ActionBarActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private LinearLayout mTestLayout;
+    private RecyclerView mRecyclerView;
+
+    public static final List<String> data;
+
+    static {
+        data = new ArrayList<String>();
+        data.add("浮夸 - 陈奕迅");
+        data.add("好久不见 - 陈奕迅");
+        data.add("时间都去哪儿了 - 王铮亮");
+        data.add("董小姐 - 宋冬野");
+        data.add("爱如潮水 - 张信哲");
+        data.add("给我一首歌的时间 - 周杰伦");
+        data.add("天黑黑 - 孙燕姿");
+        data.add("可惜不是你 - 梁静茹");
+        data.add("太委屈 - 陶晶莹");
+        data.add("用心良苦 - 张宇");
+        data.add("说谎 - 林宥嘉");
+        data.add("独家记忆 - 陈小春");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +56,49 @@ public class MainActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mRecyclerView = (RecyclerView)findViewById(R.id.main_recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
         mDrawerToggle.syncState();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         mTestLayout = (LinearLayout)findViewById(R.id.test_layout);
+        mRecyclerView.setAdapter(new MyAdapter(data));
 
     }
 
+    private static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private List<String> data;
+        public MyAdapter(List<String> data) {
+            this.data = data;
+        }
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView)itemView.findViewById(R.id.main_item_textview);
+            }
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_content_item, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.mTextView.setText(data.get(position));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
