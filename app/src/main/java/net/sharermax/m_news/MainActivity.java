df8cbde0fe0880904b9eb03a3dc2068d06ca0b62
SharerMax <mdcw1103@gmail.com>
2015-02-19 20:33:43 +0800

    Initialize M-News project

---
+++ app/src/main/java/net/sharermax/m_news/MainActivity.java
@@ -0,0 +1,68 @@
+package net.sharermax.m_news;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static String TAG = "MainActivty";
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private LinearLayout mTestLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mTestLayout = (LinearLayout)findViewById(R.id.test_layout);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                if (mDrawerLayout.isDrawerVisible(mTestLayout)) {
+                    mDrawerLayout.closeDrawer(mTestLayout);
+                } else {
+                    mDrawerLayout.openDrawer(mTestLayout);
+                }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
