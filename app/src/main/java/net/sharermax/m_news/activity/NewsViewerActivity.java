8a045ed578e0dde39d3a87e2f33f441e8b7122b8
SharerMax <mdcw1103@gmail.com>
2015-06-22 16:43:48 +0800

    add option menu for reload current webpage and fix bug "Uncaught TypeError: Cannot read property 'getItem' of null"

--- app/src/main/java/net/sharermax/m_news/activity/NewsViewerActivity.java
+++ app/src/main/java/net/sharermax/m_news/activity/NewsViewerActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -43,7 +45,7 @@ public class NewsViewerActivity extends AbsActivity {
         ButterKnife.inject(this);
         ViewCompat.setElevation(mHeaderView, getResources().getDimension(R.dimen.toolbar_elevation));
         Bundle args = getIntent().getExtras();
-        String title = "TTTTT";
+        String title = "";
         String url = "http://saeratom.us";
         if (args != null) {
             title = args.getString(FLAG_EXTRA_TITLE);
@@ -54,7 +56,10 @@ public class NewsViewerActivity extends AbsActivity {
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                mWebView.loadUrl(url);
+                if (BuildConfig.DEBUG) {
+                    Log.v(CLASS_NAME, "load url:" + url);
+                }
+                view.loadUrl(url);
                 return false;
             }
         });
@@ -69,7 +74,27 @@ public class NewsViewerActivity extends AbsActivity {
                 if (100 == newProgress) mLoadProgressBar.setVisibility(View.GONE);
             }
         });
-
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
         mWebView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.newsviewer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_action_refresh:
+                mLoadProgressBar.setVisibility(View.VISIBLE);
+                mLoadProgressBar.setProgress(0);
+                mWebView.reload();
+                return true;
+            default:
+                return false;
+        }
     }
 }
